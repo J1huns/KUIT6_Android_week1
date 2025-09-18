d97922611a7b7d704e05b318eb17326819d05e4a
J1huns <eksdidqufrhr@daum.net>
2025-09-19 07:06:22 +0900

    test commit

--- app/src/main/java/com/example/kuit6_week1/MainActivity.kt
+++ app/src/main/java/com/example/kuit6_week1/MainActivity.kt
@@ -4,13 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.kuit6_week1.ui.theme.Kuit6_week1Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +38,125 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Kuit6_week1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Sample(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.padding(top = 27.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+        SampleRow {
+            Box(
+                modifier = modifier
+                    .width(width = 60.dp)
+                    .height(height = 60.dp)
+                    .clip(shape = RoundedCornerShape(size = 20.dp))
+                    .background(Color.Black)
+
+            )
+            Box(
+                modifier = modifier
+                    .width(width = 60.dp)
+                    .height(height = 60.dp)
+                    .clip(CircleShape)
+                    .background(Color.Black)
+
+            )
+        }
+        SampleRow(modifier = Modifier.padding(top = 20.dp)) {
+
+            Box(
+                modifier = modifier
+                    .size(size = 60.dp)
+                    .border(
+                        width = 2.dp,
+                        color = Color.Black,
+                        shape = RoundedCornerShape(size = 20.dp)
+                    )
+            )
+            Box(
+                modifier = modifier
+                    .size(size = 60.dp)
+                    .border(
+                        width = 3.dp,
+                        color = Color.Black,
+                        shape = CircleShape
+                    )
+            )
+
+        }
+        SampleRow(modifier = Modifier.padding(top = 20.dp)
+        ) {
+            Box(
+                modifier = modifier
+                    .size(size = 76.dp)
+                    .border(
+                        width = 1.dp,
+                        color = Color.Black
+                    )
+                    .padding(all = 8.dp)
+                    .background(Color.Red)
+
+            )
+            Box(
+                modifier = modifier
+                    .size(size = 76.dp)
+                    .border(
+                        width = 1.dp,
+                        color = Color.Black
+                    )
+                    .padding(all = 8.dp)
+                    .clip(CircleShape)
+                    .background(Color.Blue)
+
+
+            )
+        }
+        SampleRow(modifier = Modifier.padding(all = 20.dp)){
+            Image(
+                painter = painterResource(R.drawable.image),
+                contentDescription = "개 이미지",
+                modifier = Modifier.size(size = 80.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.image),
+                contentDescription = "개 이미지",
+                modifier = Modifier
+                    .size(size = 80.dp)
+                    .clip(CircleShape)
+            )
+        }
+
+        Text(
+            modifier = Modifier
+                .padding(top = 20.dp)
+                .fillMaxWidth()
+                .border(width = 2.dp, color = Color.Black)
+                .padding(all = 10.dp),
+
+            text = "Hello World!",
+            fontSize = 20.sp,
+            fontStyle = FontStyle.Normal,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            textAlign = TextAlign.Center
+        )
+    }
 }
+
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Kuit6_week1Theme {
-        Greeting("Android")
+private fun SamplePreview() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Sample()
     }
 }
